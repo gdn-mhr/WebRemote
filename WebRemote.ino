ecee0176a744f4f63740dceb44d5809db03fd82e
Gideon mohr <56309634+gdn-mhr@users.noreply.github.com>
2021-03-06 22:59:24 +0100

    Added board config
    Please refer to this https://github.com/tykefcz/IniIno for instruction on how to install it.

--- WebRemote.ino
+++ WebRemote.ino
@@ -1,3 +1,6 @@
+/* Boards:
+ * - WeMos D1 R1 * esp8266:esp8266:d1:baud=921600,xtal=160,eesz=4M2M,dbg=Disabled,lvl=None____,ip=hb2f,vt=flash,exception=legacy,wipe=none,ssl=all,CONSOLEBAUD=115200
+ */
 #include "WebRemote.h"
 
 std::queue<Event*> eventqueue;
